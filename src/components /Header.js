457afed4565ca5a934903a6ee56c018881a9e417
Dan Possehl <danielbpossehl@gmail.com>
2022-07-29 14:49:03 -0500

    layout stuff

---
+++ src/components /Header.js
@@ -0,0 +1,54 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 140px;
+  width: 100vw;
+  display: flex;
+  background-color: white;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  height: 100%;
+  width: 35%;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Right = styled.div`
+  height: 100%;
+  width: 40%;
+  background-color: white;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const TreeSection = styled.div`
+  height: 400px;
+  width: 100vw;
+  background-color: red;
+`;
+
+const header = () => {
+  return (
+    <div>
+      <Container>
+        <Left>
+          <div>LOGO</div>
+        </Left>
+        <Right>
+          <div>Thing</div>
+          <div>Thing</div>
+          <div>Thing</div>
+          <div>Thing</div>
+        </Right>
+      </Container>
+    </div>
+  );
+};
+
+export default header;
--- src/components /SectionOne.js
+++ src/components /SectionOne.js
@@ -1,16 +1,64 @@
 import React from "react";
 import styled from "styled-components";
+import Header from "./Header";
 
 const MainContainer = styled.div`
   height: 100vh;
   width: 100vw;
   background-color: pink;
+  display: flex;
+  flex-direction: column;
+`;
+
+const TreeSection = styled.div`
+  height: 500px;
+  width: 100vw;
+  background-color: red;
+`;
+
+const BottomContainer = styled.div`
+  height: 310px;
+  width: 100vw;
+  background-color: white;
+  display: flex;
+`;
+
+const BottomLeft = styled.div`
+  height: 100%;
+  width: 33%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const BottomMiddle = styled.div`
+  height: 100%;
+  width: 33%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const BottomRight = styled.div`
+  height: 100%;
+  width: 33%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const SectionOne = () => {
   return (
     <>
-      <MainContainer></MainContainer>
+      <MainContainer>
+        <Header></Header>
+        <TreeSection></TreeSection>
+        <BottomContainer>
+          <BottomLeft>Test</BottomLeft>
+          <BottomMiddle>Test</BottomMiddle>
+          <BottomRight>Test</BottomRight>
+        </BottomContainer>
+      </MainContainer>
     </>
   );
 };
